ba838698c7b539bc8c15df7b9947600255ad25e4
2mehali <2mehalipahari@gmail.com>
2022-06-06 04:41:53 +0530

    first commit

---
+++ src/main/java/com/shoes_Sporty/repository/PurchaseReportRepository.java
@@ -0,0 +1,21 @@
+package com.shoes_Sporty.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.shoes_Sporty.model.PurchaseReport;
+
+@Repository
+public interface PurchaseReportRepository extends JpaRepository<PurchaseReport, Integer> {
+
+	@Query("select pr from PurchaseReport pr where pr.categoryOfProduct=:category")
+	List<PurchaseReport> findAllByCategory(String category);
+
+	@Query("select pr from PurchaseReport pr where pr.dateOfProductPurchase=:date")
+	List<PurchaseReport> findAllByDate(Date date);
+
+}
